70f86b3a91c3890c2f5821ace16c68ab010ad84b
HRMxan <hrmxan@gmail.com>
2022-05-14 22:16:26 +0500

    yangi sahifalar

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,5 @@
+AOS.init();
+
 let colapseButton = document.getElementById('colapseButton');
 let navbarColapse = document.getElementById('navbarColapse');
 
@@ -27,9 +29,11 @@ pause.forEach(element => {
         video.pause();
     });
 });
+
 let languageId = document.getElementById('languageId');
 let lang_values = document.querySelectorAll('.lang_values');
 let langVal = document.getElementById('langVal');
+
 langVal.innerHTML = lang_values[0].innerHTML;
 languageId.addEventListener('click', function() {
     languageId.classList.toggle('opened');
@@ -58,20 +62,41 @@ card_sum_buttons.forEach(element => {
 let delayu_dobro = document.querySelectorAll('.delayu_dobro');
 let actions = document.getElementById('actions');
 let krestikFix = document.getElementById('krestikFix');
+let krestik = document.querySelectorAll('.krestik'); 
+let fixCard1 = document.getElementById('fixCard1');
+let myfix_container = document.querySelectorAll('.myfix_container');
+let kupitTurSum = document.getElementById('kupitTurSum');
+let buyYurDate = document.getElementById('buyYurDate');
+let delayDobroCard = document.getElementById('delayDobroCard');
+
 delayu_dobro.forEach(element => {
-    element.addEventListener('click', function() {
+    element.addEventListener('click', function(e) {
+        e.preventDefault();
         actions.classList.add('fix_on');
+        delayDobroCard.classList.add('card_fix');
         document.body.style.overflow = 'hidden';
     });
 });
-krestikFix.addEventListener('click', function(e) {
-    actions.classList.remove('fix_on');
-    document.body.style.overflow = 'auto';
-});
-let buy_tur_actions = document.querySelectorAll('.buy_tur_actions');
-buy_tur_actions.forEach(element => {
-    element.addEventListener('click', function() {
-        actions.classList.add('fix_on');
-        document.body.style.overflow = 'hidden';
+krestik.forEach(element => {
+    element.addEventListener('click', function(e) {
+        e.preventDefault();
+        actions.classList.remove('fix_on');
+        document.body.style.overflow = 'auto';
+        myfix_container.forEach(element => {
+            element.classList.remove('card_fix');
+        });
     });
+});
+// moted a tag href event
+buyYurDate.addEventListener('click', function(e) {
+    e.preventDefault();
+    actions.classList.add('fix_on');
+    fixCard1.classList.add('card_fix');
+    document.body.style.overflow = 'hidden';
+});
+kupitTurSum.addEventListener('click', function(e) {
+    e.preventDefault();
+    actions.classList.add('fix_on');
+    fixCard2.classList.add('card_fix');
+    document.body.style.overflow = 'hidden';
 });
